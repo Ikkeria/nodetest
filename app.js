0a3ffe49e7bf663cd6f3f0aee502bf18b43f112b
Abrahamsson Rune (DFDS A/S) <ruabr@DFDS.COM>
2017-05-29 09:36:56 +0200

    reverting to http

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-'use strict';
-
-let spdy = require('spdy'),
-    fs = require('fs');
-    
-let options = {
-    key: fs.readFileSync(__dirname + '/server.key'),
-    cert: fs.readFileSync(__dirname + '/server.crt')
-};
-
-spdy.createServer(options, function(req, res) {
-    res.writeHead(200);
-    res.end('Hello world over HTTP/2');
-}).listen(3000);
+var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);

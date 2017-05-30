491c807f9b62dc24e0ebead81ac215b331864e97
Sebastian Hallén <sebastian.hallen@tretton37.com>
2017-05-30 11:15:33 +0200

    configurable port

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ var options = {
     cert: fs.readFileSync(__dirname + '/cert.pem'),
     passphrase: 'testtest1234'
 };
-const port = 8080;
+const PORT = process.env.PORT || 8080;
 
 try {
-  console.log(`starting server on port: ${port}`);
+  console.log(`starting server on port: ${PORT}`);
 
   require('http2').createServer(options, function(request, response) {
     console.log('http/2 request received');
     response.end('Hellold! From HTTP2.');
-  }).listen(port);
+  }).listen(PORT);
 
 } catch (error) {
   console.log(`application exploded: ${error}`);

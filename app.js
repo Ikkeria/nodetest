fecc2660d6eb4169b8fe48944c3b80fba4883c01
Sebastian Hallén <sebastian.hallen@tretton37.com>
2017-05-30 10:09:51 +0200

    add some more logging for debugging purposes

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ var options = {
     cert: fs.readFileSync(__dirname + '/cert.pem'),
     passphrase: 'testtest1234'
 };
+const port = 8080;
 
-require('http2').createServer(options, function(request, response) {
-  response.end('Hellold! From HTTP2.');
-}).listen(8080);
+try {
+  console.log(`starting server on port: ${port}`);
+
+  require('http2').createServer(options, function(request, response) {
+    console.log('http/2 request received');
+    response.end('Hellold! From HTTP2.');
+  }).listen(port);
+
+} catch (error) {
+  console.log(`application exploded: ${error}`);
+}
